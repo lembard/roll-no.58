a363948248b78a97f2106911797f087b83813e0e
lembard <bhellpuri9@gmail.com>
2025-08-04 10:35:21 +0530

    Add files via upload

---
+++ lab4/Part-c/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int arr[] = {1,2,3,4,5},i;
+
+    cout<<"TRAVERSAL OF AN ARRAY : \n";
+    for(i = 0;i < 5;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
